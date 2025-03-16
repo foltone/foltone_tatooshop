a67d4fb4fc5ec8c99b4d20530d53947b5d2aaeb6
foltone <foltonedev@gmail.com>
2025-03-16 17:27:22 +0100

    first commit

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales["en"] = {
+    ["subtitle_menu"] = "Tattoo Menu",
+    ["rotate_left"] = "Rotate left",
+    ["rotate_right"] = "Rotate right",
+    ["press_access"] = "Press ~INPUT_CONTEXT~ to access the tattoo shop",
+    ["hair"] = "Hair",
+    ["you_bought_tattoo"] = "You bought a tattoo for ~g~%s$",
+    ["not_enough_money"] = "~r~ You don't have enough money.",
+    ["you_removed_tattoo"] = "You removed a tattoo",
+}
